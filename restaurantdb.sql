be1995436730be4ae7b1b67a9f20b8285d88bab8
Canh Huynh <158004919+canhhuynh220805@users.noreply.github.com>
2026-04-05 09:44:30 +0700

    Add files via upload

--- restaurantdb.sql
+++ restaurantdb.sql
@@ -1,81 +1,272 @@
-create table user(
-	id int auto_increment primary key,
-	username varchar(50) not null,
-	password varchar(255),
-	name nvarchar(50) not null,
-	phone varchar(12) not null,
-	avatar varchar(255) not null,
-	user_role enum("ROLE_USER", "ROLE_ADMIN", "ROLE_CHEF")
-);
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: restaurantdb
+-- ------------------------------------------------------
+-- Server version	8.0.42
 
-create table category(
-	id int auto_increment primary key,
-	name nvarchar(50) not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table restaurant_table(
-	id int auto_increment primary key,
-	capacity int default 2,
-	table_number varchar(10) not null
-);
+--
+-- Table structure for table `category`
+--
 
-create table reservation(
-	id int auto_increment primary key,
-	user_id int,
-	table_id int,
-	start_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	end_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-	number_people int not null,
-	status enum("CONFIRMED", "CANCELED", "PENDING"),
-	foreign key(table_id) references restaurant_table(id),
-	foreign key(user_id) references user(id)
-);
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table dish(
-	id int auto_increment primary key,
-	user_id int,
-	category_id int,
-	name nvarchar(100) not null,
-	description text not null,
-	image varchar(255) not null,
-	price int default 0,
-	time_prepate int not null,
-	material text not null,
-	foreign key(user_id) references user(id),
-	foreign key(category_id) references category(id)
-);
+--
+-- Dumping data for table `category`
+--
 
-create table rating(
-	id int auto_increment primary key,
-	user_id int,
-	dish_id int,
-    content Text null,
-	point int not null,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-	foreign key(user_id) references user(id),
-	foreign key(dish_id) references dish(id)
-);
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Món Chính'),(2,'Tráng Miệng'),(3,'Nước Uống');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table orders(
-	id int auto_increment primary key,
-	user_id int,
-	reservation_id int UNIQUE NULL,
-	created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-	payment_method enum("MOMO", "ZALO_PAY", "CASH"),
-	total_price int default 0,
-	status_pay enum("CANCELED", "PENDING", "COMPLETED"),
-	status_order enum("PENDING", "COMPLETED"),
-	foreign key(user_id) references user(id),
-	foreign key(reservation_id) references reservation(id)
-);
+--
+-- Table structure for table `dish`
+--
 
-create table order_detail(
-	id int auto_increment primary key,
-	order_id int,
-	dish_id int,
-	quantity int default 1,
-	unit_price int not null,
-	foreign key(order_id) references orders(id),
-	foreign key(dish_id) references dish(id)
-);
+DROP TABLE IF EXISTS `dish`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dish` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `name` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `price` int DEFAULT '0',
+  `time_prepate` int NOT NULL,
+  `material` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `dish_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `dish_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish`
+--
+
+LOCK TABLES `dish` WRITE;
+/*!40000 ALTER TABLE `dish` DISABLE KEYS */;
+INSERT INTO `dish` VALUES (1,4,1,'Bò Bít Tết','Thăn ngoại bò tươi','https://res.cloudinary.com/dpl8syyb9/image/upload/q_auto/f_auto/v1775356448/Screenshot_2026-04-05_093350_kggvyu.png',250000,20,'Thịt bò, muối, tiêu, bơ'),(2,5,2,'Bánh Flan','Bánh mềm béo ngậy','https://res.cloudinary.com/dpl8syyb9/image/upload/q_auto/f_auto/v1775356448/Screenshot_2026-04-05_093338_rnktnb.png',35000,10,'Trứng, sữa, caramel'),(3,4,3,'Nước Ép Cam','Cam vắt tươi','https://res.cloudinary.com/dpl8syyb9/image/upload/q_auto/f_auto/v1775356448/Screenshot_2026-04-05_093324_u5pfqp.png',40000,5,'Cam tươi, đường');
+/*!40000 ALTER TABLE `dish` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_detail`
+--
+
+DROP TABLE IF EXISTS `order_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_detail` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int DEFAULT NULL,
+  `dish_id` int DEFAULT NULL,
+  `quantity` int DEFAULT '1',
+  `unit_price` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_id` (`order_id`),
+  KEY `dish_id` (`dish_id`),
+  CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_detail_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dish` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+LOCK TABLES `order_detail` WRITE;
+/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
+INSERT INTO `order_detail` VALUES (1,1,1,1,250000),(2,1,3,1,40000),(3,2,2,2,35000);
+/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `reservation_id` int DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `payment_method` enum('MOMO','ZALO_PAY','CASH') DEFAULT NULL,
+  `total_price` int DEFAULT '0',
+  `status_pay` enum('CANCELED','PENDING','COMPLETED') DEFAULT NULL,
+  `status_order` enum('PENDING','COMPLETED') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `reservation_id` (`reservation_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,2,1,'2026-04-04 21:39:23','MOMO',290000,'COMPLETED','COMPLETED'),(2,3,2,'2026-04-04 21:39:23','CASH',70000,'PENDING','PENDING');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rating` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `dish_id` int DEFAULT NULL,
+  `point` int NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `content` text,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `dish_id` (`dish_id`),
+  CONSTRAINT `rating_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `rating_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dish` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+INSERT INTO `rating` VALUES (1,2,1,5,'2026-04-04 21:39:23','Bò mềm, sốt ngon'),(2,3,2,4,'2026-04-04 21:39:23','Bánh hơi ngọt');
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `table_id` int DEFAULT NULL,
+  `start_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `end_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `number_people` int NOT NULL,
+  `status` enum('CONFIRMED','CANCELED','PENDING') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `table_id` (`table_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`table_id`) REFERENCES `restaurant_table` (`id`),
+  CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+INSERT INTO `reservation` VALUES (1,2,1,'2026-04-10 18:00:00','2026-04-10 20:00:00','2026-04-04 21:39:23',2,'CONFIRMED'),(2,3,2,'2026-04-11 19:00:00','2026-04-11 21:00:00','2026-04-04 21:39:23',4,'PENDING');
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant_table`
+--
+
+DROP TABLE IF EXISTS `restaurant_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `restaurant_table` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `capacity` int DEFAULT '2',
+  `table_number` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant_table`
+--
+
+LOCK TABLES `restaurant_table` WRITE;
+/*!40000 ALTER TABLE `restaurant_table` DISABLE KEYS */;
+INSERT INTO `restaurant_table` VALUES (1,2,'T01'),(2,4,'T02'),(3,6,'T03');
+/*!40000 ALTER TABLE `restaurant_table` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `phone` varchar(12) NOT NULL,
+  `avatar` varchar(255) NOT NULL,
+  `user_role` enum('ROLE_USER','ROLE_ADMIN','ROLE_CHEF') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','Nguyễn','Quản Trị','0901234567','https://res.cloudinary.com/dpl8syyb9/image/upload/v1774107595/dqu87moys1yoyhvmcjkp.png','ROLE_ADMIN'),(2,'khach1','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','Trần','Khách Một','0902345678','https://res.cloudinary.com/dpl8syyb9/image/upload/v1774107595/dqu87moys1yoyhvmcjkp.png','ROLE_USER'),(3,'khach2','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','Lê','Khách Hai','0903456789','https://res.cloudinary.com/dpl8syyb9/image/upload/v1774107595/dqu87moys1yoyhvmcjkp.png','ROLE_USER'),(4,'bep1','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','Phạm','Đầu Bếp Nhất','0904567890','https://res.cloudinary.com/dpl8syyb9/image/upload/v1774107595/dqu87moys1yoyhvmcjkp.png','ROLE_CHEF'),(5,'bep2','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','Vũ','Đầu Bếp Nhì','0905678901','https://res.cloudinary.com/dpl8syyb9/image/upload/v1774107595/dqu87moys1yoyhvmcjkp.png','ROLE_CHEF'),(6,'canh','$2a$10$ZI1RXsvWPp.uM/y2JvwsB.B2OAaXW2Hl9tPqEsnL3rCtV6iFCTVEK','canh','huynh','0969293472','https://res.cloudinary.com/dpl8syyb9/image/upload/v1775355990/l5gfpdyfajuzf1xdacoe.png','ROLE_USER');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-05  9:41:51
